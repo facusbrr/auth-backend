d8af7429b7d160a63fb0714310aa028cd1a0b629
Gayoso Facundo <facusbrr@gmail.com>
2024-09-02 12:26:34 -0300

    Se realizo la conexion a la base de datos

--- backend-jwt/config/env.js
+++ backend-jwt/config/env.js
@@ -1,2 +1,9 @@
+//Servidor
 export const PORT = process.env.PORT || 4000;
-export const SECRET_KEY = process.env.SECRET_KEY || 'seed-of-backend-jwt';
+//JWT Secret
+export const SECRET_KEY = process.env.SECRET_KEY || "seed-of-backend-jwt";
+//Databases
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "";
+export const DB_NAME = process.env.DB_PASSWORD || "db_system";
---
+++ backend-jwt/db/db.js
@@ -0,0 +1,23 @@
+import { createPool } from "mysql2/promise";
+
+export const pool = createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 50,
+  queueLimit: 1000,
+});
+
+// Obtener una conexión con el pool
+export default async function connectionDB() {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Conectado a la base de datos");
+    return connection;
+  } catch (err) {
+    console.error("No se conectó a la base de datos", err.stack);
+    throw err;
+  }
+}
