aeb883ea5b94dc8b4fbea7cd2eb677055c9675ef
Gayoso Facundo <facusbrr@gmail.com>
2024-09-04 09:33:26 -0300

    Se agrego la base de datos

--- backend-session/db/database.js
+++ backend-session/db/database.js
@@ -1,25 +1,24 @@
-export const database = 
-    {
-        user: [
-            {
-                id: 1,
-                username: 'admin',
-                password: 'admin'
-            },
-            {
-                id: 2,
-                username: 'user',
-                password: 'user'
-            },
-            {
-                id: 3,
-                username: 'guest',
-                password: 'guest'
-            },
-            {
-                id: 4,
-                username: 'guest2',
-                password: 'guest2'
-            }
-        ]
-    }
+import { createPool } from "mysql2/promise";
+import { DB_HOST, DB_PASSWORD, DB_USER, DB_NAME } from "../config/env.js";
+
+export const pool = createPool({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 50,
+  queueLimit: 1000,
+});
+
+// Obtener una conexión con el pool
+export default async function connectionDB() {
+  try {
+    const connection = await pool.getConnection();
+    console.log("Conectado a la base de datos");
+    return connection;
+  } catch (err) {
+    console.error("No se conectó a la base de datos", err.stack);
+    throw err;
+  }
+}
