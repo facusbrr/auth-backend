a2126eb1a9e20dc014be73233d7a1ffa48f9824a
Gayoso Facundo <facusbrr@gmail.com>
2024-09-05 11:47:33 -0300

    Se fixeo para obtener el session

--- frontend/main.js
+++ frontend/main.js
@@ -9,5 +9,35 @@ const $app = document.querySelector("#app");
 if (pathname === "/home") {
   $app.appendChild(Navbar());
   $app.appendChild(Home());
+  (async () => {
+    const response = await fetch("http://localhost:4000/session", {
+      method: "GET",
+      credentials: "include", // Importante para enviar las cookies de sesión
+    });
+
+    console.log({ response });
+
+    if (response.ok) {
+      const data = await response.json();
+      document.getElementById("user-name").innerText = data.user.username;
+    } else {
+      // Redirigir al usuario a la página de inicio de sesión
+      window.location.href = "index.html";
+    }
+  })();
+
+  // Manejar el cierre de sesión
+  document.getElementById("logout").addEventListener("click", async () => {
+    const response = await fetch("http://localhost:4000/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al cerrar sesión");
+    } else {
+      window.location.href = "login";
+    }
+  });
 }
 if (pathname === "/login") $app.appendChild(LoginPage());
--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", async () => {
+  // Obtener la sesión del usuario
   try {
     const response = await fetch("http://localhost:4000/session", {
       method: "GET",
       credentials: "include", // Importante para enviar las cookies de sesión
     });
+    console.log({ response });
     if (response.ok) {
       const data = await response.json();
       document.getElementById("user-name").innerText = data.user.username;
@@ -11,31 +13,30 @@ document.addEventListener("DOMContentLoaded", async () => {
       // Redirigir al usuario a la página de inicio de sesión
       window.location.href = "login";
     }
-    console.log({ response });
   } catch (error) {
-    throw new Error(`Ocurrió un error en la sesión: ${error}`);
+    console.error(`Error al obtener la sesión: ${error}`);
+  }
+
+  // Manejar el cierre de sesión
+  const logoutButton = document.getElementById("logout");
+  if (logoutButton) {
+    logoutButton.addEventListener("click", async () => {
+      try {
+        const response = await fetch("http://localhost:4000/logout", {
+          method: "POST",
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+          throw new Error("Error al cerrar sesión");
+        } else {
+          window.location.href = "login";
+        }
+      } catch (error) {
+        console.error("Error al cerrar sesión:", error);
+      }
+    });
+  } else {
+    console.error("Elemento con ID 'logout' no encontrado.");
   }
 });
-
-// Manejar el cierre de sesión
-const logoutButton = document.getElementById("logout");
-if (logoutButton) {
-  logoutButton.addEventListener("click", async () => {
-    try {
-      const response = await fetch("http://localhost:4000/logout", {
-        method: "POST",
-        credentials: "include",
-      });
-
-      if (!response.ok) {
-        throw new Error("Error al cerrar sesión");
-      } else {
-        window.location.href = "login";
-      }
-    } catch (error) {
-      console.error("Error al cerrar sesión:", error);
-    }
-  });
-} else {
-  console.error("Elemento con ID 'logout' no encontrado.");
-}
